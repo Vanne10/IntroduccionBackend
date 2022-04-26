333f3922c20751f2a03472267ffa9d92beb41767
Vanne10 <vanessa.burrola.soto@gmail.com>
2022-04-26 14:34:31 -0600

    Hola Mundo en HTML usanso HTTP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Llamamos el modulo http
 const http = require('http');
 //Establecemos la url o ip de nuestro servidor
-const hostname = '192.168.50.187'//'127.0.0.1'
+const hostname = '192.168.100.22'//'127.0.0.1'
 //Establecemos el puerto de escucha
 const port = 3000;
 //Creamos una instancia http con un reques y un response
@@ -9,9 +9,9 @@ const server = http.createServer((req, res) => {
     //El servidor responderá un codigo 200
     res.statusCode = 200;
     //El servidor responderá con un texto plano
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     //El servidor responderá el mensaje Hola Mundo
-    res.end('Hola Mundo');
+    res.end('<h1>Hola Mundo</h1>');
 });
 
 server.listen(port, hostname, () => {
