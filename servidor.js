5c72231b2d5b3d233ccdf1dfd1b46d70d9287eba
Vanne10 <vanessa.burrola.soto@gmail.com>
2022-04-28 11:48:06 -0600

    Agregando los códigos de estado HTTP

--- servidor.js
+++ servidor.js
@@ -19,4 +19,26 @@ http.createServer(function (request, response) {
         '.css': 'text/css',
         '.gif': 'image/gif'
     };
-});
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error ' + error.code + '..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
